3d0924e7345d2e91e5061d749dff2271d4020555
sai <mogilisaikumar875@gmail.com>
2026-07-22 09:01:31 +0530

    Fix 500: Add collectstatic to build.sh for WhiteNoise manifest generation

--- build.sh
+++ build.sh
@@ -5,7 +5,10 @@ set -o errexit
 # 1. Install dependencies
 pip install -r requirements.txt
 
-# 2. Run migrations to create database tables
+# 2. Collect static files (required by WhiteNoise manifest storage)
+python manage.py collectstatic --no-input
+
+# 3. Run migrations to create database tables
 python manage.py migrate
 
 # 3. First admin account
